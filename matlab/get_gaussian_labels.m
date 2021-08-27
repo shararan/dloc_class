af2a8cc655ceab6732cc8688501b4396a2c3db8f
Charlie <457511811@qq.com>
2021-08-27 11:44:40 -0700

    format code

--- matlab/get_gaussian_labels.m
+++ matlab/get_gaussian_labels.m
@@ -6,14 +6,13 @@ function labels_gaussian = get_gaussian_labels(labels,sigma,x_vals,y_vals)
     map_X = repmat(x_vals,n_ylabels,1);
     map_Y = repmat(y_vals',1,n_xlabels);
     n_points = size(labels,1);
+    
     for i=1:n_points
         d = (map_X-labels(i,1)).^2+(map_Y-labels(i,2)).^2;
         cur_gaussian = exp(-d/sigma/sigma);%*1/sqrt(2*pi)/output_sigma;        
         labels_gaussian(i,:)=cur_gaussian(:);
     
     end
-
-    
 end
 
 
